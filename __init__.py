11a9628954688deaed29beda4703de7828df54ef
bitranox <rnowotny1966@gmail.com>
2019-08-27 20:26:17 +0200

    refractor

--- __init__.py
+++ __init__.py
@@ -1,2 +1,6 @@
 # this __init__.py is only meant for local package development
-from .lib_detect_encoding import *
+try:
+    from .lib_detect_encoding import *
+# this we need for pip install --install-option test
+except ImportError:
+    import lib_detect_encoding
